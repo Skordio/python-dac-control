9c628cb1064fe69e4845801adcefabd5d66d14de
Steven Wells <skordion@hotmail.com>
2023-04-18 09:14:53 -0400

    feat: python gui code working well with potentiometer mode

--- with_potentiometer/maingui.py
+++ with_potentiometer/maingui.py
@@ -3,13 +3,26 @@ import time
 import tkinter as tk
 from tkinter import messagebox
 
+potentiometer_mode = True
+potentiometer_mode_text = ''
+
+def potentiometer_mode_toggle():
+    try:
+        arduino.write("POT".encode())
+        messagebox.showinfo("Info", "Potentiometer mode activated. Set a voltage value to exit this mode.")
+    except Exception as e:
+        messagebox.showerror("Error", e)
+        
+
 def set_voltage(event=None):
     try:
         voltage = voltage_entry.get()
         if voltage == "":
+            potentiometer_mode = True
             arduino.write("POT".encode())
             messagebox.showinfo("Info", "Potentiometer mode activated. Enter a voltage value to exit this mode.")
         else:
+            potentiometer_mode = False
             voltage = float(voltage)
             if 0 <= voltage <= 4.61:
                 voltage *= 1.085
@@ -21,7 +34,7 @@ def set_voltage(event=None):
             else:
                 raise ValueError()
     except ValueError:
-        messagebox.showerror("Error", "Invalid input. Please enter a valid voltage between 0 and 4.61 volts or leave the field empty to use the potentiometer.")
+        messagebox.showerror("Error", "Invalid input. Please enter a valid voltage between 0 and 4.61 volts, or click potentiometer mode to use potentiometer.")
 
 def exit_program():
     arduino.close()
@@ -46,10 +59,13 @@ voltage_entry = tk.Entry(frame)
 voltage_entry.grid(row=0, column=1, padx=5, pady=5)
 voltage_entry.bind('<Return>', set_voltage)
 
-set_voltage_button = tk.Button(frame, text="Set Voltage", command=set_voltage)
-set_voltage_button.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
+set_potentiometer_mode_button = tk.Button(frame, text=f"Potentiometer Mode", command=potentiometer_mode_toggle)
+set_potentiometer_mode_button.grid(row=2, column=0, columnspan=1, padx=5, pady=5)
+
+set_voltage_button = tk.Button(frame, text="Click to set voltage (or press enter)", command=set_voltage)
+set_voltage_button.grid(row=1, column=0, columnspan=1, padx=5, pady=5)
 
 exit_button = tk.Button(frame, text="Exit", command=exit_program)
-exit_button.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+exit_button.grid(row=2, column=1, columnspan=1, padx=5, pady=5)
 
 root.mainloop()
